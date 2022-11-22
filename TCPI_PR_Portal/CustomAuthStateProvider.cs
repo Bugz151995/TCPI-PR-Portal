76702864d8e48ffbc97b7851d26b970aba70421c
Bugz151995 <ryan.abugao@gmail.com>
2022-11-22 14:08:44 +0800

    Test Cookie Handler for request

--- TCPI_PR_Portal/CustomAuthStateProvider.cs
+++ TCPI_PR_Portal/CustomAuthStateProvider.cs
@@ -7,10 +7,6 @@ namespace TCPI_PR_Portal.Client
     {
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            // hard coded token.
-            string token = "";
-
-            // if claims identity is empty, it means that user is not authenticated
             var identity = new ClaimsIdentity();
             var user = new ClaimsPrincipal(identity);
             var state = new AuthenticationState(user);
--- TCPI_PR_Portal/Program.cs
+++ TCPI_PR_Portal/Program.cs
@@ -1,7 +1,9 @@
+using Client.Extensions;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using System.Net;
 using TCPI_PR_Portal;
 using TCPI_PR_Portal.Client;
 
@@ -9,7 +11,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://172.31.24.203:50000") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://172.31.24.203:50000") });
+
+builder.Services.AddTransient<CookieHandler>()
+    .AddScoped(sp => sp
+        .GetRequiredService<IHttpClientFactory>()
+        .CreateClient("ServiceLayer"))
+    .AddHttpClient("ServiceLayer", client => client.BaseAddress = new Uri("https://172.31.24.203:50000")).AddHttpMessageHandler<CookieHandler>();
+
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddMudServices();
