b85890c622fe4d598ba53a6ac6c368318dea3800
Bugz151995 <ryan.abugao@gmail.com>
2022-11-23 16:36:04 +0800

    test submit form user setup

--- TCPI_PR_Portal/Data/DepartmentsDto.cs
+++ TCPI_PR_Portal/Data/DepartmentsDto.cs
@@ -10,5 +10,22 @@ namespace TCPI_PR_Portal.Models
     {
         public int Code { get; set; }
         public string Name { get; set; }
+
+        public override bool Equals(object o)
+        {
+            var other = o as DepartmentsDto;
+            return other?.Code == Code;
+        }
+        public override int GetHashCode() => Code.GetHashCode();
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class DepartmentsResponse
+    {
+        public string odata { get; set; } = string.Empty;
+        public List<DepartmentsDto> value { get; set; } = new List<DepartmentsDto>();
     }
 }
