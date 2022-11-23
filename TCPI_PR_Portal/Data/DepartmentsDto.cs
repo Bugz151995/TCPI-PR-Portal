aa707605729e0f6e52f1404319f7b123975083ad
Bugz151995 <ryan.abugao@gmail.com>
2022-11-23 15:23:16 +0800

    test

--- TCPI_PR_Portal/Data/DepartmentsDto.cs
+++ TCPI_PR_Portal/Data/DepartmentsDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TCPI_PR_Portal.Models
 {
-    public class Departments
+    public class DepartmentsDto
     {
         public int Code { get; set; }
         public string Name { get; set; }
