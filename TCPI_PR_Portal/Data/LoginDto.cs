988eef2c4325cd4df7610458534cb1235bb04943
Bugz151995 <ryan.abugao@gmail.com>
2022-11-23 19:02:10 +0800

    test user setup

---
+++ TCPI_PR_Portal/Data/LoginDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TCPI_PR_Portal.Data
+{
+    public class LoginDto
+    {
+        [Required]
+        public string U_UserName { get; set; } = string.Empty;
+        [Required]
+        public string U_Password { get; set; } = string.Empty;
+    }
+
+    public class LoginResponse
+    {
+        public string odata { get; set; } = string.Empty;
+        public List<LoginDto> value { get; set; } = new List<LoginDto>();
+    }
+}
---
+++ TCPI_PR_Portal/Data/UserDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPI_PR_Portal.Models
+{
+    public class UserDto
+    {
+        [Required]
+        public string Code { get; set; }        
+        [Required]
+        public string U_UserName { get; set; }
+        [Required]
+        public string U_Password { get; set; }
+        public string U_Employee { get; set; }
+        [EmailAddress, Required]
+        public string U_EmailAddress { get; set; }
+        [Required]
+        public string U_Role { get; set; }
+
+        public string? U_Approver1 { get; set; }
+        public string? U_Approver2 { get; set; }
+        public string? U_Approver3 { get; set; }
+        public string? U_Approver4 { get; set; }
+        public int? U_ApproverCode1 { get; set; }
+        public int? U_ApproverCode2 { get; set; }
+        public int? U_ApproverCode3 { get; set; }
+        public int? U_ApproverCode4 { get; set; }
+
+        // Uncomment if needed this is included in the mapping not in the UI
+        //public string Name { get; set; }
+        //public int U_UserId { get; set; }
+        //public string U_ApprovalLevel { get; set; }
+        //public int U_Department { get; set; }
+        //public int U_CostCenter { get; set; }
+    }
+}
