eb48a18079e1ea674327a5596f4361a71fc398df
Bugz151995 <ryan.abugao@gmail.com>
2022-11-25 22:30:19 +0800

    changed the logic behind password or account login

--- TCPI_PR_Portal/Data/LoginDto.cs
+++ TCPI_PR_Portal/Data/LoginDto.cs
@@ -15,6 +15,7 @@ namespace TCPI_PR_Portal.Data
         public string Code { get; set; }
         public string U_Employee { get; set; }
         public string U_Role { get; set; }
+        public string U_Password { get; set; }
 
         public string? U_Approver1 { get; set; }
         public string? U_Approver2 { get; set; }
