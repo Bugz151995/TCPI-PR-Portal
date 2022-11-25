1f56e538a433eae1b50213115ec840d59ceff3d4
Bugz151995 <ryan.abugao@gmail.com>
2022-11-25 17:31:57 +0800

    test auth

--- TCPI_PR_Portal/CustomAuthStateProvider.cs
+++ TCPI_PR_Portal/CustomAuthStateProvider.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.AspNetCore.Components.Authorization;
+using Solutaris.InfoWARE.ProtectedBrowserStorage.Services;
 using System.Security.Claims;
 
 namespace TCPI_PR_Portal.Client
 {
     public class CustomAuthStateProvider : AuthenticationStateProvider
     {
+        private readonly IIWLocalStorageService _localStorage;
+
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            var anonymous = new ClaimsIdentity();
+            string employeeName = await _localStorage.GetItemAsync<string>("EmployeeName");
+            string role = await _localStorage.GetItemAsync<string>("Role");
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, employeeName),
+                new Claim(ClaimTypes.Role, role)
+            };
+
+            var anonymous = new ClaimsIdentity(claims, "UserAuthentication");
             return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymous)));
         }
     }
