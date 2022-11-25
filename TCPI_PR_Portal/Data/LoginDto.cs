c790620626f924a2d083dab0e48b570d8a4b6593
Bugz151995 <ryan.abugao@gmail.com>
2022-11-25 14:02:40 +0800

    updated the session stored in clien

--- TCPI_PR_Portal/Data/LoginDto.cs
+++ TCPI_PR_Portal/Data/LoginDto.cs
@@ -14,6 +14,18 @@ namespace TCPI_PR_Portal.Data
     {
         public string Code { get; set; }
         public string U_Employee { get; set; }
+        public string U_Role { get; set; }
+
+        public string? U_Approver1 { get; set; }
+        public string? U_Approver2 { get; set; }
+        public string? U_Approver3 { get; set; }
+        public string? U_Approver4 { get; set; }
+        public string? U_ApproverSpecial { get; set; }
+        public string? U_ApproverCode1 { get; set; }
+        public string? U_ApproverCode2 { get; set; }
+        public string? U_ApproverCode3 { get; set; }
+        public string? U_ApproverCode4 { get; set; }
+        public string? U_ApproverSpecialCode { get; set; }
     }
 
     public class LoginResponse
