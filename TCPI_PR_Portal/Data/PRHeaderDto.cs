bfe967e06a9199a59a83391e4673941eb5d5ac4e
Bugz151995 <ryan.abugao@gmail.com>
2022-11-30 08:48:48 +0800

    test

--- TCPI_PR_Portal/Data/PRHeaderDto.cs
+++ TCPI_PR_Portal/Data/PRHeaderDto.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TCPI_PR_Portal.Data;
 
 namespace TCPI_PR_Portal.Models
 {
@@ -16,14 +17,14 @@ namespace TCPI_PR_Portal.Models
         public string Name { get; set; }
         public int? U_DocEntry { get; set; }
         [Required, Display(Name="Project Code")]
-        public string? U_ProjectID { get; set; } // revert back to int after testing
+        public string? U_ProjectID { get; set; }
         public string? U_ProjName { get; set; }
         public string? U_Location { get; set; }
-        public string? U_PRType { get; set; } // no binded field
+        public string? U_PRType { get; set; } 
         [Required, Display(Name="Doc Number")]
         public string? U_DocNum { get; set; }
-        public string? U_Department { get; set; } // Preferred Supplier to confirm if this is the field for it
-        public string? U_Branch { get; set; } // no binded field // list
+        public string? U_Department { get; set; } 
+        public string? U_Branch { get; set; } 
         public string? U_DocStatus { get; set; }
         public DateTime? U_TaxDate { get; set; }
         public DateTime? U_ReqDate { get; set; }
@@ -39,5 +40,13 @@ namespace TCPI_PR_Portal.Models
         public string? U_ApprovedDate { get; set; }
         public string? U_Remarks { get; set; }
         public string? U_ApproverRemarks { get; set; }
+    }
+
+    public class PRHeaderResponse
+    {
+        public string odatametadata { get; set; } = string.Empty;
+        public List<PRHeaderDto> value { get; set; } = new List<PRHeaderDto>();
+        public Error error { get; set; } = new Error();
+        public string odatanextLink { get; set; } = string.Empty;
     }
 }
